b792c1214465004496dc4dea2393de8fa908928b
Erik Escobedo <erik@mojotech.com>
2012-05-02 21:00:19 -0500

    Kick off.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monociclo::Application.config.secret_token = '4d3d21e55d7b7917f4406a5e75da28c069c4bd6a9e1ac0a3464519adfbfa6d17e8e678d2a08e1e060b90a7241ca1a4e219aeb70bd65087d1331be036652fd901'
