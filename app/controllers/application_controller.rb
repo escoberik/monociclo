ad1f7d21e701919c44aa3203344e78e201d127ad
Erik Escobedo <erik@mojotech.com>
2012-05-07 15:09:59 -0500

    Basic HTTP authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :basic_authentication
+
+  def basic_authentication
+    authenticate_or_request_with_http_basic do |user, password|
+      user == 'sergio' && password == 'glenn_jade'
+    end
+  end
 end
