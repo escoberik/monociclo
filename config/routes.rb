ea73a5e082abda1b713af74680ae38999387d699
Erik Escobedo <erik.escobedo.e@gmail.com>
2012-05-07 14:29:19 -0500

    Finished application home as static_pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Monociclo::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
 
+
+
+match "prensa" => "static_pages#prensa", :as => :prensa
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -48,7 +49,7 @@ Monociclo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'static_pages#index'
 
   # See how all your routes lay out with "rake routes"
 
