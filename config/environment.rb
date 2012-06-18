804b59823fc48ec54ff7229d38752d73a90179ca
Erik Escobedo <erik@mojotech.com>
2012-06-18 17:01:20 -0500

    Define app_name before loading everything.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,4 @@
+module Rails; def self.app_name; Rails.application.class.parent_name.downcase; end; end
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
--- config/initializers/rails.rb
+++
@@ -1,5 +0,0 @@
-module Rails
-  def self.app_name
-    Rails.application.class.parent_name.downcase
-  end
-end
